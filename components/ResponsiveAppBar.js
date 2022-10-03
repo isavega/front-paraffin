ced9f75c506957707350fb62f42d46e307e0ae43
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-03 15:06:21 -0300

    feat: modify names and typos

--- components/ResponsiveAppBar.js
+++ components/ResponsiveAppBar.js
@@ -1,24 +1,24 @@
-import * as React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import { useRouter } from "next/router";
 
 const pages = ["About us", "Roadmaps"];
-const settings = ["Profile", "Account", "Logout"];
+const settings = ["Logout"];
 
 const ResponsiveAppBar = () => {
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElNav, setAnchorElNav] = useState(null);
+	const [anchorElUser, setAnchorElUser] = useState(null);
+	const router = useRouter();
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
@@ -35,11 +35,20 @@ const ResponsiveAppBar = () => {
 		setAnchorElUser(null);
 	};
 
+	const submitHandler = (string) => {
+		console.log("ACAAAAAAAAAAAAAAAAAAA");
+		console.log(string);
+		if (string === "About us") {
+			router.push("/about");
+		} else {
+			router.push("/roadmaps");
+		}
+	};
+
 	return (
 		<AppBar position="static" style={{ backgroundColor: "#424242" }}>
 			<Container maxWidth="xl">
 				<Toolbar disableGutters>
-					{/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
 					<Typography
 						variant="h6"
 						noWrap
@@ -49,71 +58,17 @@ const ResponsiveAppBar = () => {
 							mr: 2,
 							display: { xs: "none", md: "flex" },
 							fontFamily: "monospace",
-							fontWeight: 700,
+							fontWeight: 900,
 							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
 						}}>
 						🔥 PARAFFIN 🔥
 					</Typography>
 
-					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleOpenNavMenu}
-							color="inherit">
-							<MenuIcon />
-						</IconButton>
-						<Menu
-							id="menu-appbar"
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: "bottom",
-								horizontal: "left",
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: "top",
-								horizontal: "left",
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: "block", md: "none" },
-							}}>
-							{pages.map((page) => (
-								<MenuItem key={page} onClick={handleCloseNavMenu}>
-									<Typography textAlign="center">{page}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
-					<AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href=""
-						sx={{
-							mr: 2,
-							display: { xs: "flex", md: "none" },
-							flexGrow: 1,
-							fontFamily: "monospace",
-							fontWeight: 700,
-							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
-						}}>
-						PARAFFIN
-					</Typography>
 					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
 						{pages.map((page) => (
 							<Button
 								key={page}
-								onClick={handleCloseNavMenu}
+								onClick={() => submitHandler(page)}
 								sx={{ my: 2, color: "white", display: "block" }}>
 								{page}
 							</Button>
@@ -142,7 +97,7 @@ const ResponsiveAppBar = () => {
 							open={Boolean(anchorElUser)}
 							onClose={handleCloseUserMenu}>
 							{settings.map((setting) => (
-								<MenuItem key={setting} onClick={handleCloseUserMenu}>
+								<MenuItem key={setting}>
 									<Typography textAlign="center">{setting}</Typography>
 								</MenuItem>
 							))}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import AppBar from "../components/AppBar";
-import BasicCard from "../components/BasicCard";
-
+import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import Home from "./home";
 
 export default function Index() {
-	// const [data, setData] = useState();
-
-	// useEffect(() => {
-	// 	const getCurriculums = async () => {
-	// 		const response = await fetch("/api/curriculums");
-	// 		const getResponse = await response.json();
-	// 		setData(getResponse);
-	// 	};
-	// 	getCurriculums();
-	// }, []);
-
 	return (
 		<div className={styles.container}>
+			<ResponsiveAppBar />
 			<Home />
 		</div>
 	);
