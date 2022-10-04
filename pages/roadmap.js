a5f996d43333cd4c1d4a7431636aaad26d30df06
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-04 14:54:42 -0300

    fix: change name for better understanding

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -4,55 +4,55 @@ import RoadmapCard from "../components/RoadmapCard/RoadmapCard";
 import axios from "axios";
 
 function Roadmap() {
-  const router = useRouter();
-  const {
-    id: roadmapId,
-    name: roadmapName,
-    description: roadmapDescription,
-  } = router.query;
-  const [data, setData] = useState();
+	const router = useRouter();
+	const {
+		id: roadmapId,
+		name: roadmapName,
+		description: roadmapDescription,
+	} = router.query;
+	const [data, setData] = useState();
 
-  useEffect(() => {
-    const getCheckpoints = async () => {
-      try {
-        const response = await axios.get(
-          `/api/curriculums/${roadmapId}/learning_units`
-        );
-        setData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getCheckpoints();
-  }, []);
+	useEffect(() => {
+		const getCheckpoints = async () => {
+			try {
+				const response = await axios.get(
+					`/api/curriculums/${roadmapId}/learning_units`
+				);
+				setData(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		getCheckpoints();
+	}, []);
 
-  const dataHandler = (currentCheckpoint) => {
-    router.push(
-      {
-        pathname: "/checkpoint",
-        query: {
-          ...currentCheckpoint,
-          roadmapId: roadmapId,
-        },
-      },
-      "/checkpoint"
-    );
-  };
+	const dataHandler = (currentCheckpoint) => {
+		router.push(
+			{
+				pathname: "/checkpoint",
+				query: {
+					...currentCheckpoint,
+					roadmapId: roadmapId,
+				},
+			},
+			"/checkpoint"
+		);
+	};
 
-  return (
-    <div>
-      <h1>{roadmapName}</h1>
-      <p>{roadmapDescription}</p>
-      {data?.map((checkpoint, i) => (
-        <RoadmapCard
-          key={checkpoint.id}
-          title={checkpoint.name}
-          description={checkpoint.description}
-          action={() => dataHandler(checkpoint)}
-          style={{ margin: "10%" }}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			<h1>{roadmapName}</h1>
+			<p>{roadmapDescription}</p>
+			{data?.map((checkpoint, i) => (
+				<RoadmapCard
+					key={checkpoint.id}
+					title={checkpoint.name}
+					description={checkpoint.description}
+					action={() => dataHandler(checkpoint)}
+					style={{ margin: "10%" }}
+				/>
+			))}
+		</div>
+	);
 }
 export default Roadmap;
