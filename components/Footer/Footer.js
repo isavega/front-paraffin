fe70a776d129288d661a349257f87b6b51e5476d
isavega <igvega02@gmail.com>
2022-10-04 23:27:31 -0300

    feat: add footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        marginRight: "5%",
+        display: "flex",
+        justifyContent: "flex-end",
+      }}
+    >
+      <p>Made with ❤️ by 👑</p>
+    </div>
+  );
+};
+
+export default Footer;
