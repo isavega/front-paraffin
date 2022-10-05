c7bfefd4a38356ec4f1ac34f130e9324386ec08d
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-05 11:44:10 -0300

    fix: delete non used functions

--- components/ResponsiveAppBar/ResponsiveAppBar.js
+++ components/ResponsiveAppBar/ResponsiveAppBar.js
@@ -21,109 +21,114 @@ const pages = ["About us", "Roadmap"];
 const settings = ["Logout"];
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-  const router = useRouter();
+	const [anchorElNav, setAnchorElNav] = useState(null);
+	const [anchorElUser, setAnchorElUser] = useState(null);
+	const router = useRouter();
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+	const chulengoAvatar =
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4wUZNkVsvfl0pfnaAEJaa4oeOD7uipWJSG76sv5uN6UdAHADnO-XpW_eYcMczRLwJQZA&usqp=CAU";
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
-  const submitLogout = async (userId) => {
-    const response = await signOut(1); //TODO manage id
-    if (response.code === 200) {
-      router.push("/login");
-      return;
-    }
-    setShowAlert(true);
-    setTimeout(() => setShowAlert(false), 3000);
-  };
+	const submitLogout = async (userId) => {
+		const response = await signOut(1); //TODO manage id
+		console.log(response);
+		if (response.code === 200) {
+			router.push("/login");
+			return;
+		}
+		setShowAlert(true);
+		setTimeout(() => setShowAlert(false), 3000);
+	};
 
-  const submitHandler = (string) => {
-    if (string === "About us") {
-      router.push("/about");
-    } else {
-      router.push("/roadmap");
-    }
-  };
+	const submitHandler = (string) => {
+		if (string === "About us") {
+			router.push("/about");
+		} else {
+			router.push("/home");
+		}
+	};
 
-  return (
-    <AppBar position="static" style={{ backgroundColor: "#424242" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 900,
-              letterSpacing: ".3rem",
-            }}
-          >
-            🔥 PARAFFIN 🔥
-          </Typography>
+	return (
+		<AppBar position="static" style={{ backgroundColor: "#424242" }}>
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography
+						variant="h6"
+						noWrap
+						component="a"
+						href="/"
+						sx={{
+							mr: 2,
+							display: { xs: "none", md: "flex" },
+							fontFamily: "monospace",
+							fontWeight: 900,
+							letterSpacing: ".3rem",
+						}}>
+						🔥 PARAFFIN 🔥
+					</Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => submitHandler(page)}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						{pages.map((page) => (
+							<Button
+								key={page}
+								onClick={() => submitHandler(page)}
+								sx={{ my: 2, color: "white", display: "block" }}>
+								{page}
+							</Button>
+						))}
+					</Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={chulengoAvatar} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting}>
-                  <Typography textAlign="center" onClick={submitLogout}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						{pages.map((page) => (
+							<Button
+								key={page}
+								onClick={() => submitHandler(page)}
+								sx={{ my: 2, color: "white", display: "block" }}>
+								{page}
+							</Button>
+						))}
+					</Box>
+
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title="Open settings">
+							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+								<Avatar alt="Remy Sharp" src={chulengoAvatar} />
+							</IconButton>
+						</Tooltip>
+						<Menu
+							sx={{ mt: "45px" }}
+							id="menu-appbar"
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}>
+							{settings.map((setting) => (
+								<MenuItem key={setting}>
+									<Typography textAlign="center" onClick={submitLogout}>
+										{setting}
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 };
 export default ResponsiveAppBar;
