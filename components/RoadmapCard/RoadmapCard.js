7cdd2f9009246cde3881379ef4db9f446a28fdcd
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-05 11:44:32 -0300

    fix: change name roadmapcard

--- components/RoadmapCard/RoadmapCard.js
+++ components/RoadmapCard/RoadmapCard.js
@@ -6,23 +6,23 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 function RoadmapCard(props) {
-  const { title, description, action } = props;
-  return (
-    <Card sx={{ minWidth: 275 }} style={{ borderBlockColor: "gray" }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
-        <Typography variant="body2">{description}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" style={{ color: "gray" }} onClick={action}>
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+	const { title, description, action } = props;
+	return (
+		<Card sx={{ minWidth: 275 }} style={{ borderBlockColor: "gray" }}>
+			<CardContent>
+				<Typography variant="h5" component="div">
+					{title}
+				</Typography>
+				<Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
+				<Typography variant="body2">{description}</Typography>
+			</CardContent>
+			<CardActions>
+				<Button size="small" style={{ color: "gray" }} onClick={action}>
+					Learn More
+				</Button>
+			</CardActions>
+		</Card>
+	);
 }
 
 export default RoadmapCard;
