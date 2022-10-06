09de9c2fbd0f01c76664ee9a6391e8a94391cdb3
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-06 18:37:44 -0300

    fix: url change

--- api/api.js
+++ api/api.js
@@ -1,33 +1,33 @@
 import axios from "axios";
 
 export const postResource = async (data) => {
-  try {
-    const response = await axios.post(
-      `api/curriculums/${data.curriculumId}/learning_units/${data.learningUnitId}/resources/create`,
-      data
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const response = await axios.post(
+			`api/curriculums/${data.curriculumId}/learning_units/${data.learningUnitId}/resources/create`,
+			data
+		);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const postComment = async (data) => {
-  try {
-    const response = await axios.post(
-      `api/curriculums/${data.roadmapId}/learning_units/${data.checkpointId}/resources/${data.resourceId}/comments`,
-      data
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const response = await axios.post(
+			`api/curriculums/${data.roadmapId}/learning_units/${data.checkpointId}/resources/${data.resourceId}/comments`,
+			data
+		);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const postCompletedCheckpoint = async (data) => {
 	try {
 		const response = await axios.post(
-			`api/curriculums/${data.curriculumId}/learning_units/${data.learningUnitId}/set_completed`,
+			`api/curriculums/${data.curriculumId}/learning_units/${data.learningUnitId}/completed_learning_unit`,
 			data
 		);
 		console.log("Changed!");
