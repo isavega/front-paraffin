6599e8f7ceb9e569f6717c6d76b5262b137da685
isavega <igvega02@gmail.com>
2022-10-07 16:46:43 -0300

    feat: add style to checkpoints

--- pages/checkpoint.js
+++ pages/checkpoint.js
@@ -4,79 +4,89 @@ import RoadmapCard from "../components/RoadmapCard/RoadmapCard";
 import axios from "axios";
 import Button from "@mui/material/Button";
 import { postCompletedCheckpoint } from "../api/api";
+import globalStyle from "../styles/globalStyle";
+import Confetti from "react-confetti";
 
 function Checkpoint() {
-	const router = useRouter();
-	const [data, setData] = useState();
-	const [completed, setCompleted] = useState("Completar");
-	const {
-		roadmapId,
-		learning_unit_id: checkpointId,
-		name: checkpointName,
-		description: checkpointDescription,
-	} = router.query;
+  const router = useRouter();
+  const [data, setData] = useState();
+  const [confetti, setConfetti] = useState(false);
+  const [completed, setCompleted] = useState("Completar");
+  const {
+    roadmapId,
+    id: checkpointId,
+    name: checkpointName,
+    description: checkpointDescription,
+  } = router.query;
 
-	useEffect(() => {
-		const getResources = async () => {
-			try {
-				const response = await axios.get(
-					`/api/curriculums/${roadmapId}/learning_units/${checkpointId}/resources`
-				);
-				setData(response.data);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		getResources();
-	}, []);
+  useEffect(() => {
+    const getResources = async () => {
+      try {
+        const response = await axios.get(
+          `/api/curriculums/${roadmapId}/learning_units/${checkpointId}/resources`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getResources();
+  }, []);
 
-	const checkHandler = () => {
-		if (completed === "Completar") {
-			setCompleted("Completado!");
-			console.log(completed);
-			postCompletedCheckpoint({
-				learning_unit_id: checkpointId,
-				learningUnitId: checkpointId,
-				curriculumId: roadmapId,
-				user_id: 1,
-			});
-		} else {
-			setCompleted("Completar");
-		}
-	};
+  const checkHandler = () => {
+    if (completed === "Completar") {
+      setCompleted("Completado!");
+      setConfetti(true);
+      postCompletedCheckpoint({
+        learning_unit_id: checkpointId,
+        learningUnitId: checkpointId,
+        curriculumId: roadmapId,
+        user_id: 1,
+      });
+      setTimeout(() => setConfetti(false), 5000);
+    } else {
+      setCompleted("Completar");
+    }
+  };
 
-	const dataHandler = (currentResource) => {
-		router.push(
-			{
-				pathname: "/resource",
-				query: {
-					...currentResource,
-					roadmapId: roadmapId,
-					checkpointId: checkpointId,
-				},
-			},
-			"/resource"
-		);
-	};
+  const dataHandler = (currentResource) => {
+    router.push(
+      {
+        pathname: "/resource",
+        query: {
+          ...currentResource,
+          roadmapId: roadmapId,
+          checkpointId: checkpointId,
+        },
+      },
+      "/resource"
+    );
+  };
 
-	return (
-		<div>
-			<h1>{checkpointName}</h1>
-			<p>{checkpointDescription}</p>
-			<Button variant="contained" onClick={checkHandler}>
-				{completed}
-			</Button>
-			<div>
-				{data?.map((resource, index) => (
-					<RoadmapCard
-						key={resource.id}
-						title={resource.name}
-						description={resource.description}
-						action={() => dataHandler(resource)}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div style={globalStyle.grid.container}>
+      {confetti && <Confetti width={2560} height={1600} />}
+      <h1 style={globalStyle.font.title}>{checkpointName}</h1>
+      <p style={globalStyle.font.body}>{checkpointDescription}</p>
+
+      {data && (
+        <div style={globalStyle.grid.container}>
+          <RoadmapCard
+            title={data[0].name}
+            description={data[0].description}
+            buttonText="Ver recurso"
+            buttonAction={() => dataHandler(data[0])}
+          />
+          <Button
+            variant="contained"
+            style={globalStyle.button}
+            onClick={checkHandler}
+          >
+            {completed}
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Checkpoint;
