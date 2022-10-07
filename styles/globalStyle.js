7d4d78e1c2baff8f4d6e709d0b6529996945425c
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-10-07 16:52:57 -0300

    fix: about us style

--- styles/globalStyle.js
+++ styles/globalStyle.js
@@ -1,34 +1,34 @@
 import color from "../styles/color";
 
 const globalStyle = {
-  font: {
-    title: {
-      fontSize: 35,
-      color: color.black,
-      alignSelf: "center",
-    },
-    body: {
-      fontSize: 22,
-      color: color.grey[300],
-      alignSelf: "center",
-    },
-  },
-  grid: {
-    container: {
-      width: "100%",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "flex-start",
-    },
-  },
-  button: {
-    color: color.white,
-    backgroundColor: color.black,
-    alignSelf: "center",
-    width: "50%",
-    margin: "8%",
-    borderRadius: "10px",
-  },
+	font: {
+		title: {
+			fontSize: 35,
+			color: color.black,
+			alignSelf: "center",
+		},
+		body: {
+			fontSize: 22,
+			color: color.grey[300],
+			alignSelf: "center",
+		},
+	},
+	grid: {
+		container: {
+			width: "100%",
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "flex-start",
+		},
+	},
+	button: {
+		color: color.white,
+		backgroundColor: color.black,
+		alignSelf: "center",
+		width: "50%",
+		margin: "8%",
+		borderRadius: "10px",
+	},
 };
 
 export default globalStyle;
